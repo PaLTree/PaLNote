4c9e109414cad45f4aa6cebc289e27784fd76c94
snvx <snovxn@gmail.com>
2014-10-01 17:53:43 +0900

    Windowクラスの追加
    まだ何もない

---
+++ src/main/java/org/paltree/palnote/view/Window.java
@@ -0,0 +1,18 @@
+/**
+ * PaLNote
+ * https://github.com/PaLTree/PaLNote
+ *
+ * Copyright (c) 2014 PaLTree. All rights reserved.
+ * 　Released under the Apache v2 License.
+ * 　https://github.com/PaLTree/PaLNote/blob/master/LICENSE
+ *
+ * Created by snvx on 14/10/01.
+ */
+package org.paltree.palnote.view;
+
+/**
+ * MainWindowClass
+ */
+public class Window {
+
+}
