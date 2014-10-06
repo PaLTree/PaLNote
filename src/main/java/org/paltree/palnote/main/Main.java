eb910d2115ef5a27663c824c60aa234bd68359cb
snvx <snovxn@gmail.com>
2014-10-06 22:59:11 +0900

    startクラスの修正とmainクラスの追加
    FXMLLoaderの追加、Overrideアノテーションを消していたので再び追加

--- src/main/java/org/paltree/palnote/main/Main.java
+++ src/main/java/org/paltree/palnote/main/Main.java
@@ -11,17 +11,20 @@
 package org.paltree.palnote.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public void start(Stage stage) {
-        HBox root = new HBox();
-        Scene scene = new Scene(root, 800, 600);
-
-        stage.setTitle("PaLNote");
-        stage.setScene(scene);
-        stage.show();
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Window.fxml"));
+        primaryStage.setTitle("PaLNote");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
