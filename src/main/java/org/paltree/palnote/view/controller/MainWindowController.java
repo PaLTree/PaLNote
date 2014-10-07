da2b273487aa6d4a813d5551027c972c1601539a
snvx <snovxn@gmail.com>
2014-10-07 23:14:55 +0900

    onAboutAppとonPreferencesメソッドを追加

--- src/main/java/org/paltree/palnote/view/controller/MainWindowController.java
+++ src/main/java/org/paltree/palnote/view/controller/MainWindowController.java
@@ -14,12 +14,26 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import org.paltree.palnote.view.AboutApp;
 
 public class MainWindowController {
     @FXML private MenuItem about_app;
     @FXML private MenuItem check_updates;
     @FXML private MenuItem preferences;
     @FXML private MenuItem quit_application;
+
+    @FXML
+    public void onAboutApp (ActionEvent event) {
+        Stage aboutStage = new AboutApp(stage);
+        aboutStage.show();
+    }
+
+    @FXML
+    public void onPreferences (ActionEvent event) {
+
+    }
 
     @FXML
     public void onQuitApplication (ActionEvent event) {
