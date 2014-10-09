0f68f4f18f831171ecb8602e86ab5e8605e0be1e
snvx <snovxn@gmail.com>
2014-10-09 21:30:37 +0900

    PreferenceWindowクラスの追加

---
+++ src/main/java/org/paltree/palnote/view/window/PreferenceWindow.java
@@ -0,0 +1,30 @@
+/**
+ * PaLNote
+ * https://github.com/PaLTree/PaLNote
+ *
+ * Copyright (c) 2014 PaLTree. All rights reserved.
+ * 　Released under the Apache v2 License.
+ * 　https://github.com/PaLTree/PaLNote/blob/master/LICENSE
+ *
+ * Created by snvx on 14/10/09.
+ */
+package org.paltree.palnote.view.window;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class PreferenceWindow extends Stage {
+
+    public PreferenceWindow () {
+        initModality(Modality.APPLICATION_MODAL);
+        initStyle(StageStyle.UTILITY);
+        setTitle("Preference");
+        setResizable(false);
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
+        setScene(scene);
+    }
+}
