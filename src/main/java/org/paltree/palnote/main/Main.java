db57186fe50748709bf1b26235e9571b32372eab
snvx <snovxn@gmail.com>
2014-10-14 22:14:26 +0900

    細かな修正

--- src/main/java/org/paltree/palnote/main/Main.java
+++ src/main/java/org/paltree/palnote/main/Main.java
@@ -18,12 +18,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Window.fxml"));
         primaryStage.setTitle("PaLNote");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
