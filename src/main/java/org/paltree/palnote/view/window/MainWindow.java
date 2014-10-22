b97eb491053aa827b0e92b33bab329f8e610430d
snvx <snovxn@gmail.com>
2014-10-22 20:38:11 +0900

    MainWindowをコード化した
    まだ完全に途中、(めんどくさいなんて言えない)

--- src/main/java/org/paltree/palnote/view/window/MainWindow.java
+++ src/main/java/org/paltree/palnote/view/window/MainWindow.java
@@ -10,9 +10,66 @@
  */
 package org.paltree.palnote.view.window;
 
+import javafx.application.Platform;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
 /**
  * MainWindowClass
  */
-public class MainWindow {
+public class MainWindow extends Stage {
+    public MainWindow() {
+        final VBox vBox = new VBox();
+        //メニューバー
+        MenuBar menuBar = new MenuBar();
+        menuBar.isUseSystemMenuBar();
 
+        Menu menuPaLNote = new Menu("PaLNote");
+        MenuItem aboutApp = new MenuItem("About PaLNote");
+        aboutApp.setOnAction((event) -> {
+                    Stage aboutStage = new AboutAppWindow();
+                    aboutStage.show();
+                }
+        );
+        MenuItem checkUpdate = new MenuItem("Check for Updates");
+        MenuItem preferences = new MenuItem("Preferences...");
+        preferences.setOnAction((event) -> {
+                    Stage preferenceWindow = new PreferenceWindow();
+                    preferenceWindow.show();
+                }
+        );
+        MenuItem quitApplication = new MenuItem("QuitApplication");
+        quitApplication.setOnAction((event) -> Platform.exit());
+
+        menuPaLNote.getItems().addAll(aboutApp, checkUpdate, preferences, quitApplication);
+
+        Menu menuFile = new Menu("File");
+        MenuItem newFolder = new MenuItem("NewFolder");
+        MenuItem openFolder = new MenuItem("OpenFolder");
+        MenuItem newFile = new MenuItem("NewFile");
+
+        menuFile.getItems().addAll(newFolder, openFolder, newFile);
+
+        Menu menuEdit = new Menu("Edit");
+
+        Menu menuView = new Menu("View");
+        Menu menuCode = new Menu("Code");
+        menuBar.getMenus().addAll(menuPaLNote, menuFile, menuEdit, menuView, menuCode);
+
+        TabPane tabPane = new TabPane();
+        Tab tab = new Tab();
+        tab.setGraphic(new ImageView("@../images/programming_language/js.png"));
+        HBox hBox = new HBox();
+        hBox.getChildren().add(new TextArea());
+        tab.setContent(hBox);
+        tabPane.getTabs().addAll(tab);
+
+        vBox.getChildren().addAll(tabPane);
+
+    }
 }
