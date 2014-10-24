e773aa734ce2eee451115872b14156709f28b5d0
snvx <snovxn@gmail.com>
2014-10-25 00:02:36 +0900

    AlertとDialogのクラスを追加

---
+++ src/main/java/org/paltree/palnote/view/window/Dialog.java
@@ -0,0 +1,17 @@
+/*
+ * PaLNote
+ * https://github.com/PaLTree/PaLNote
+ *
+ * Copyright (c) 2014 PaLTree. All rights reserved.
+ * 　Released under the Apache v2 License.
+ * 　https://github.com/PaLTree/PaLNote/blob/master/LICENSE
+ *
+ * Created by snvx on 14/10/24.
+ */
+package org.paltree.palnote.view.window;
+
+public class Dialog {
+    public void dialog() {
+
+    }
+}
